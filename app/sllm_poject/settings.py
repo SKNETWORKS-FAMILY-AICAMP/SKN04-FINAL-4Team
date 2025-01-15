d2c15c203c3cb380a38c73ceba981c286496f72b
말랑곰 <leehoje000@gmail.com>
2025-01-15 16:52:19 +0900

    feat: 장고와 리액트 연결을 위한 CORS추가
    장고와 리액트의 포트번호가 달라도 요청이 가능하도록 corsheaders 추가

--- app/sllm_poject/settings.py
+++ app/sllm_poject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_filters",
     "rest_framework",
+    "corsheaders",
     "history",
     "api",
 ]
@@ -55,7 +56,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "sllm_poject.urls"
 
