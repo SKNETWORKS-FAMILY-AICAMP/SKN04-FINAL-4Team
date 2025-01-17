0423d0bdb808d139b1c0d91ae8761dd3058cedb8
말랑곰 <leehoje000@gmail.com>
2025-01-17 14:06:23 +0900

    update: 토큰 라이프타임 수정
    토큰의 라이프타임을 1시간에서 1일로 수정

--- app/sllm_poject/settings.py
+++ app/sllm_poject/settings.py
@@ -70,8 +70,8 @@ else:
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(weeks=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
